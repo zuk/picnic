662411fab7a821cf96ba52a9d42d5d15ff57f1d5
notroot <notroot@ubuntu.localdomain>
2009-04-21 02:47:16 -0400

    Update for newer camping to provide compatibility with Passenger 2.1+.

--- lib/picnic.rb
+++ lib/picnic.rb
@@ -2,7 +2,7 @@ require 'rubygems'
 require 'activesupport'
 
 $: << File.dirname(__FILE__)
-$: << File.dirname(__FILE__) + "/../vendor/camping-2.0.20090212/lib"
+$: << File.dirname(__FILE__) + "/../vendor/camping-2.0.20090421/lib"
 
 require "camping"
 
